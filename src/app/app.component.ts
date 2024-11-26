92fed668ccd38622d8aacb7e8faf440b33d02e12
socuser1 <socuser1@domain.local>
2024-11-26 15:17:38 +0800

    Personal and education

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonalComponent } from './personal/personal.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { TrainingComponent } from './training/training.component';
+import { ReferenceComponent } from './reference/reference.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonalComponent, EducationComponent, ExperienceComponent,TrainingComponent, ReferenceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
